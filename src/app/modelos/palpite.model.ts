2774e72744616382f7c8518eb4c436838d87ffca
wendellpaim <125922277+wendellpaim@users.noreply.github.com>
2023-02-20 17:34:41 -0300

    Update palpite.model.ts

--- src/app/modelos/palpite.model.ts
+++ src/app/modelos/palpite.model.ts
@@ -2,11 +2,13 @@ import { Evento } from "./evento.model"
 import { Usuario } from "./usuario.model"
 
 export class Palpite {
-   palpite: Array<number>=[] // posição zero seleção 1, posição 1 seleção 2.
+   palpite1:number=0
+   palpite2:number=0// posição1 seleção 1, posição2 seleção 2.
    usuario_palpite!: Usuario 
    evento_palpite!: Evento
 
-   constructor(palpite:Array<number>){
-    this.palpite=palpite
+   constructor(palpite1:number,palpite2=number){
+    this.palpite1=palpite1
+    this.palpite2=palpite2
    }
 }
