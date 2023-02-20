2023cabe981e9248d7d1ffe6342ac6b364ced17f
wendellpaim <125922277+wendellpaim@users.noreply.github.com>
2023-02-20 17:52:58 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventoListaComponent } from './components/evento-lista/evento-lista.component';
+import { EventoComponent } from './components/evento/evento.component';
 import { LoginComponent } from './components/login/login.component';
+import { PalpiteListaComponent } from './components/palpite-lista/palpite-lista.component';
+import { PalpiteComponent } from './components/palpite/palpite.component';
+import { RankingComponent } from './components/ranking/ranking.component';
 import { SelecaoListaComponent } from './components/selecao-lista/selecao-lista.component';
+import { UsuarioListaComponent } from './components/usuario-lista/usuario-lista.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { Selecao } from './modelos/selecao.model';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
-  {path:"selecao/listar", component:SelecaoListaComponent}
+  {path:"selecao/listar", component:SelecaoListaComponent},
+  {path:"selecao",component:Selecao},
+  {path:"evento/listar", component:EventoListaComponent},
+  {path:"evento",component:EventoComponent},
+  {path:"palpite",component:PalpiteComponent},
+  {path:"palpite/listar",component:PalpiteListaComponent},
+  {path:"ranking",component:RankingComponent},
+  {path:"usuario",component:UsuarioComponent},
+  {path:"usuario/listar",component:UsuarioListaComponent}
+  
+
   
 ];
 
